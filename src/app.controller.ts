25e8fc465547f70d70679123e4c1cff763d62f8e
Vivek Kushwah <vivek@beakon.com.au>
2023-12-15 16:39:18 +0530

    my commit

---
+++ src/app.controller.ts
@@ -0,0 +1,49 @@
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('/register')
+  @HttpCode(200)
+  registerUser(@Body() data){
+    console.log(data)
+    return this.appService.registerUser(data)
+  }
+
+
+  @Post('/addBook')
+  @HttpCode(200)
+  addBook(@Body() data){
+    console.log(data)
+   return this.appService.addBook(data)
+  }
+
+  @Post('/registerBookUser')
+  registerBookUser(@Body() data){
+    return this.appService.registerBookUser(data)
+  }
+
+  @Post('/addMonthlySubs')
+  addMonthlySubscription(@Body() data){
+    return this.appService.addMonthlySubscription(data)
+  }
+
+  @Post('/addPayment')
+  addPayment(@Body() data){
+    return this.appService.addPayment(data)
+  }
+
+  @Get('/users')
+  getAllUsers(){
+    return this.appService.getAllUsers()
+  }
+
+
+}
---
+++ src/app.service.ts
@@ -0,0 +1,124 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Injectable()
+export class AppService {
+
+  constructor(private prisma: PrismaService) {
+
+  }
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  async addBook(bookData) {
+    let output = await this.prisma.book.create({
+      data: {
+        title: bookData.title,
+        price: bookData.price,
+        year: bookData.year
+      }
+    })
+    return output
+  }
+
+  async addBookUser(bookData) {
+    let output = await this.prisma.book.create({
+      data: {
+        title: bookData.title,
+        price: bookData.price,
+        year: bookData.year
+      }
+    })
+    return output
+  }
+
+  async registerUser(userData) {
+    let output = await this.prisma.adminUser.create({
+      data: userData
+    })
+    return output
+  }
+
+  async registerBookUser(userData) {
+    let output = await this.prisma.bookUser.create({
+      data: userData
+    })
+    return output
+  }
+
+  async addMonthlySubscription(userData) {
+    let output = await this.prisma.monthlySubsription.create({
+      data: {
+        month: userData.month,
+        year: userData.year,
+        bookUser: {
+          connect: {
+            id: userData.userID
+          }
+        },
+        book: {
+          connect: {
+            id: userData.bookID
+          }
+        }
+      }
+    })
+    return output
+  }
+
+  async addPayment(paymentData) {
+    let output = await this.prisma.payment.create({
+      data: {
+        bookUser: {
+          connect: {
+            id: paymentData.userID
+          }
+        },
+        book: {
+          connect: {
+            id: paymentData.bookID
+          }
+        },
+        isPaid: 1
+      }
+    })
+    return output
+  }
+
+
+  async getAllUsers() {
+
+
+    let output = await this.prisma.bookUser.findMany({
+      include: {
+        payment: true, monthlySubscription: true
+      },
+    })
+
+    let userID = output.map((x => x.id))
+
+    let temp = await this.prisma.monthlySubsription.findMany({
+      select: { book: true, userID: true },
+      where: { userID: { in: userID } }
+    })
+
+    for (let i = 0; i < output.length; i++) {
+      let mIndex = temp.filter((x => x.userID == output[i].id))
+      if (mIndex.length > 0) {
+        let newData = mIndex.map((x => x.book))
+        
+        //* filter unique books and assign it to book key
+        output[i]['books'] = newData.filter((value, index, self) =>
+        self.findIndex(v => v.id === value.id) === index
+      )
+      }
+    }
+
+    console.log(temp)
+
+    return output
+  }
+
+}
